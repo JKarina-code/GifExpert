5472827f8ec00ad93cc8760a91e95d4e29472f4b
Jymma <jkmogollon.cp@gmail.com>
2023-06-14 14:07:08 -0500

    Cleaning code in GifExpert.jsx

--- src/GifExpert.jsx
+++ src/GifExpert.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory,GifGrid } from "./components";
+
+
+
+
 export const GifExpert = () => {
   const [categories, setCategories] = useState(["Dragon Ball", "Pokemon"]);
   const onAddCategory = (newCategory) => {
