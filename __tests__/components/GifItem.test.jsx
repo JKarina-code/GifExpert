9897fb07f5a69a39ac5056d4903cb85dce2d9d6b
Jymma <jkmogollon.cp@gmail.com>
2023-06-26 13:10:21 -0500

    Test in getGifs in helpers

--- __tests__/components/GifItem.test.jsx
+++ __tests__/components/GifItem.test.jsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
-describe("test in <GifItem/>", () => {
-  const title = "Pokemon";
-  const url ="https://user-images.githubusercontent.com/29663094/234428463-bc23cfcb-c5fb-4b77-82b4-e56e6f8fa9fc.png";
 
-  test("must do match with snapshot", () => {
+describe("Tests in <GifItem/>", () => {
+  const title = "Pokemon";
+  const url =
+    "https://user-images.githubusercontent.com/29663094/234428463-bc23cfcb-c5fb-4b77-82b4-e56e6f8fa9fc.png";
+
+  test("Should do match with snapshot", () => {
     const { container } = render(<GifItem title={title} src={url} />);
 
     expect(container).toMatchSnapshot();
   });
 
-  test("must show the image with src and alt", () => {
+  test("Should show the image with src and alt", () => {
     render(<GifItem title={title} url={url} />);
 
     const { src, alt } = screen.getByRole("img");
@@ -18,7 +20,7 @@ describe("test in <GifItem/>", () => {
     expect(alt).toBe(alt);
   });
 
-  test("must  show the title", () => {
+  test("Should  show the title", () => {
     render(<GifItem title={title} url={url} />);
     expect(screen.getByText(title)).toBeTruthy();
   });
